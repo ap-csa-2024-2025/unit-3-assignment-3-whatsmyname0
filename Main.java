2af49c9626edf9c5748491beb1f23dc60341597a
Whatsmyname0 <mikepham2007>
2024-10-29 10:17:27 -0700

    yes

--- Main.java
+++ Main.java
@@ -4,11 +4,21 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // TODO: Problem 1
+    Scanner sc = new Scanner(System.in);
+    int x1 = sc.nextInt();
+    int x2 = sc.nextInt();
+     
+    if (x2 == 0)
+    {
+      System.out.println("ERROR DIVIDING BY 0");
+    }
+    else if (((x1 / x2) <= 8) && ((x1 / x2) > 1));
+    {
+      System.out.println("Ratio OK");
+    }
 
 
-    // TODO: Problem 2
-
+    int y1 = 
 
     // TODO: Problem 3
   }
